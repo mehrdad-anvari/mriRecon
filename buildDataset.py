99adad0e6966588271ab0695922d9d166290d404
MehrdadAnvari <mehrdad.anvarifard@gmail.com>
2023-07-03 16:31:29 +0330

    updated

--- buildDataset.py
+++ buildDataset.py
@@ -3,20 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-save_dir = 'J:/Anvari/preprocessedDataset2/'
-ls_files_T1 = glob.glob('J:/Anvari/data/BRATS2015_Training/*/*/*MR_T1.*/*.mha')
-ls_files_OT = glob.glob('J:/Anvari/data/BRATS2015_Training/*/*/*OT.*/*.mha')
 
-dataUnit = np.zeros((257,257,11))
+save_dir = '/content/BRATS2015_Training_Processed'
+ls_files_T1 = glob.glob('/content/BRATS2015_Training/*/*/*MR_T1.*/*.mha')
+ls_files_T2 = glob.glob('/content/BRATS2015_Training/*/*/*MR_T1.*/*.mha')
+ls_files_OT = glob.glob('/content/BRATS2015_Training/*/*/*OT.*/*.mha')
+
+dataUnit = np.zeros((257,257,7))
 
 for i in range(len(ls_files_OT)):
     
     img_T1,header = load(ls_files_T1[i])
+    img_T2,header = load(ls_files_T2[i])
     img_OT,header = load(ls_files_OT[i])
 
     img_T1 = img_T1/np.max(img_T1)
+    img_T2 = img_T2/np.max(img_T2)
 
     img_T1 = np.pad(img_T1,((8,9),(8,9),(0,0)))
+    img_T2 = np.pad(img_T2,((8,9),(8,9),(0,0)))
     img_OT = np.pad(img_OT,((8,9),(8,9),(0,0)))
 
     for j in range(2,img_T1.shape[2]-2):
@@ -27,6 +32,13 @@ for i in range(len(ls_files_OT)):
             dataUnit[:,:,1] = img_T1[:,:,j]
             dataUnit[:,:,2] = img_T1[:,:,j+1]
 
-            dataUnit[:,:,3] = img_OT[:,:,j]
+            dataUnit[:,:,3] = img_T2[:,:,j-1]
+            dataUnit[:,:,4] = img_T2[:,:,j]
+            dataUnit[:,:,5] = img_T2[:,:,j+1]
 
-            save(dataUnit, save_dir + f'{i}_{j}.mha', use_compression = True)
+            dataUnit[:,:,6] = img_OT[:,:,j]
+
+            if i < 265:
+                save(dataUnit, save_dir + '/train/' + f'{i}_{j}.mha', use_compression = True)
+            else:
+                save(dataUnit, save_dir + '/test/'  + f'{i}_{j}.mha', use_compression = True)
